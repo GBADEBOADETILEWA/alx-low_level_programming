5a45228d5aca60711ceeccd46f1e677909eb6548
Gbadebo Adetilewa <Gbadeboadetilewa@gmail.com>
2022-09-10 20:28:48 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - print possible combination of two 2-digit
- * numbers
- *
- * Return: returns zero at the end
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, p;
-	for (i = 0; i < 100; i++)
-	{
-		for (p = 0; p < 100; j++)
-		{
-			if (p > i)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
-				if (i != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+}
+putchar('\n');
+return (0);
+}
