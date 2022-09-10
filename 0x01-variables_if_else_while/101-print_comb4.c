f07acf7127ea99d5291cdeca36e0b4f9224ae300
Gbadebo Adetilewa <Gbadeboadetilewa@gmail.com>
2022-09-10 20:26:17 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - print single digit numbers starting from 0
- *
- * Return: returns zero at the end
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
+int n, m, l;
+for (n = 48; n < 58; n++)
 {
-for (j = 1; j <= 9; j++)
+for (m = 49; m < 58; m++)
 {
-if (j > i)
+for (l = 50; l < 58; l++)
 {
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8)	
+if (l > m && m > n)
+{
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
