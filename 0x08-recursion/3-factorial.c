58d57c5bc8320ee53d1c6922c0247c5b403bad97
Gbadebo Adetilewa <Gbadeboadetilewa@gmail.com>
2022-09-29 05:06:31 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,28 +1,19 @@
-/*                                                                                                                                      
- * 3-factorial.c                                                                                                                        
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * factorial - Returns the factorial of a given number.                                                                                 
- * @n: The number to find the factorial of.                                                                                             
- *                                                                                                                                      
- * Return: If n > 0 - the factorial of n.                                                                                               
- *         If n < 0 - 1 to indicate an error.                                                                                           
- */                                                                                                                                     
-int factorial(int n)                                                                                                                    
-{                                                                                                                                       
-        int result = n;                                                                                                                 
-                                                                                                                                        
-        if (n < 0)                                                                                                                      
-                return (-1);                                                                                                            
-                                                                                                                                        
-        else if (n >= 0 && n <= 1)                                                                                                      
-                return (1);                                                                                                             
-                                                                                                                                        
-        result *= factorial(n - 1);                                                                                                     
-                                                                                                                                        
-        return (result);                                                                                                                
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number to be used
+ *
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+	int next_factorial;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	next_factorial = factorial(n - 1);
+	return (n * next_factorial);
 }
